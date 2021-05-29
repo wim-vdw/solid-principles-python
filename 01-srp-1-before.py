96c9e91acffca44a5875126d546137b4e486c3dd
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2021-05-29 14:21:41 +0200

    initial commit

---
+++ 01-srp-1-before.py
@@ -0,0 +1,36 @@
+"""
+Before implementation of SRP (Single Responsibility Principle).
+
+Low cohesion between the different methods of the Person class, getting and setting property data
+has nothing to do with printing or saving the data.
+Tight-coupling in the print and save methods, in case we would like to print to a different format or
+would like to save the data to a database the complete methods needs to be rewritten.
+"""
+
+
+class Person:
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+
+    def get_first_name(self):
+        return self._first_name
+
+    def get_last_name(self):
+        return self._last_name
+
+    def get_full_name(self):
+        return self.get_last_name() + ', ' + self.get_first_name()
+
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+
+    def set_last_name(self, last_name):
+        self._last_name = last_name
+
+    def print(self):
+        print(f'Full name: {self.get_full_name()}')
+
+    def save(self):
+        with open('data.txt', 'w') as writer:
+            writer.write(self.get_full_name())
---
+++ README.md
@@ -0,0 +1,16 @@
+# SOLID principles with Python examples.
+## SRP: Single Responsibility Principle
+Every software component should have one and only one responsibility or should have only one reason to change.
+>Aim for high cohesion and loose coupling.
+
+Cohesion is the degree to which the various parts of a software component are related.  
+Coupling is defined as the level of inter-dependency between various software components.
+## OCP: Open/Closed Principle
+Software components should be closed for modification but open for extension.
+## LSP: Liskov Substitution Principle
+Objects should be replaceable with their subtypes without affecting the correctness of the program. 
+## ISP: Interface Segregation Principle
+No client should be forced to depend on methods it does note use.
+## DIP: Dependency Inversion Principle
+High-level modules should not depend on low-level modules. Both should depend on abstractions.  
+Abstractions should not depend on details. Details should depend on abstractions.
